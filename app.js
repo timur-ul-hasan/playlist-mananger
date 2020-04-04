b4dbaee100507c48df79a73e33400910770784ae
Hasan Javed <hj@embrace-it.com>
2020-04-04 22:38:11 +0500

    Authentication completed.

--- app.js
+++ app.js
@@ -1,135 +1,148 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const user = require('./user');
-const serveStatic = require('serve-static');
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const user = require("./user");
+const serveStatic = require("serve-static");
+require('dotenv').config();
 //Loads the handlebars module
-const handlebars = require('express-handlebars');
+const handlebars = require("express-handlebars");
 
 require("./setup.js");
 
 const port = 8080;
-app.set('port', port);
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/components`);
+app.set("port", port);
+app.set("view engine", "handlebars");
+app.set("views", `${__dirname}/components`);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({
-  key: 'user_sid',
-  secret: 's3cr3t',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    expires: 600000
-  }
-}));
+app.use(
+  session({
+    key: "user_sid",
+    secret: "s3cr3t",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 600000
+    }
+  })
+);
 
-app.use(serveStatic(path.join(__dirname, 'public')));
+app.use(serveStatic(path.join(__dirname, "public")));
 
 //Sets our app to use the handlebars engine
-app.engine('.hbs', handlebars({
-  layoutsDir: `${__dirname}/components`,
-  extname: '.hbs',
-  defaultLayout : "layout"
-}));
-app.set('view engine', '.hbs');//Sets handlebars configurations (we will go through them later on)
-
+app.engine(
+  ".hbs",
+  handlebars({
+    layoutsDir: `${__dirname}/components`,
+    extname: ".hbs",
+    defaultLayout: "layout"
+  })
+);
+app.set("view engine", ".hbs"); //Sets handlebars configurations (we will go through them later on)
 
 app.use((req, res, next) => {
   if (req.cookies.user_sid && !req.session.user) {
-    res.clearCookie('user_sid');
-    res.clearCookie('jwt');
+    res.clearCookie("user_sid");
+    res.clearCookie("jwt");
   }
   next();
 });
 
 const checkSession = (req, res, next) => {
   if (req.session.user && req.cookies.user_sid) {
-    res.redirect('/profile');
+    res.redirect("/profile");
   } else {
     next();
   }
 };
 
-app.get('/', (req, res) => res.render('index', {
-  user: req.session.user ? req.session.user : null,
-  intro: 'Welcome to ACME Inc., please login to view our employees. If you do not have an account, please register for one.'
-}));
-
-app.route('/register')
-.get(checkSession, (req, res) => {
-  res.render('register');
-})
-.post((req, res) => {
-  user.insertUser(req.body.username, req.body.password)
-  .then(user => {
-    if (user) {
-      req.session.user = user[0];
-      return res.redirect('/profile');
-    } else {
-      return res.render('register', {
-        error: 'User already registered.'
-      });
-    }
-  }).catch(error => {
-    console.log(error);
-    return res.redirect('/register');
+app.get("/", (req, res) => {
+  return res.render("index", {
+    user: req.session.user ? req.session.user : null,
+    intro:
+      "Welcome to ACME Inc., please login to view our employees. If you do not have an account, please register for one."
   });
 });
 
-app.route('/login')
-.get(checkSession, (req, res) => {
-  res.render('login')
-})
-.post((req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  user.checkUser(username, password)
-  .then(response => {
-    if (response) {
-      req.session.user = response;
-      const token = user.createToken(response.username);
-      res.cookie('jwt', token);
-      return res.redirect('/profile');
-    } else {
-      return res.render('login', {
-        error: 'Incorrect login details. Maybe try to <a href="/register">Register</a>.'
-      });
-    }
+app
+  .route("/register")
+  .get(checkSession, (req, res) => {
+    res.render("register");
   })
-  .catch(error => {
-    console.log(error);
-    return res.redirect('/register');
-  });
-});
-
-app.get('/profile', (req, res) => {
-  if (req.session.user && req.cookies.user_sid) {
-      res.render('profile', {
-        user: req.session.user ? req.session.user : null
+  .post((req, res) => {
+    user
+      .insertUser(req.body.username, req.body.password)
+      .then(user => {
+        if (user) {
+          req.session.user = user[0];
+          return res.redirect("/profile");
+        } else {
+          return res.render("register", {
+            error: "User already registered."
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        return res.redirect("/register");
       });
-  } else {
-      res.redirect('/login');
-  }
-});
+  });
 
-app.get('/logout', (req, res) => {
+app
+  .route("/login")
+  .get(checkSession, (req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    user
+      .checkUser(username, password)
+      .then(response => {
+        if (response) {
+          req.session.user = response;
+          const token = user.createToken(response.username);
+          res.cookie("jwt", token);
+          return res.redirect("/profile");
+        } else {
+          return res.render("login", {
+            error:
+              'Incorrect login details. Maybe try to <a href="/register">Register</a>.'
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        return res.redirect("/register");
+      });
+  });
+
+app.get("/profile", (req, res) => {
   if (req.session.user && req.cookies.user_sid) {
-    res.clearCookie('user_sid');
-    res.clearCookie('jwt');
-    res.redirect('/');
+    res.render("profile", {
+      user: req.session.user ? req.session.user : null
+    });
   } else {
-    res.redirect('/');
+    res.redirect("/login");
   }
 });
 
+app.get("/logout", (req, res) => {
+  if (req.session.user && req.cookies.user_sid) {
+    res.clearCookie("user_sid");
+    res.clearCookie("jwt");
+    res.redirect("/");
+  } else {
+    res.redirect("/");
+  }
+});
 
 app.use((req, res, next) => {
-  res.set('Content-Type', 'text/plain');
+  res.set("Content-Type", "text/plain");
   // courtesy of https://www.asciiart.eu/movies/star-wars
   res.status(404).send(`
 
@@ -164,7 +177,9 @@ app.use((req, res, next) => {
                 \ \`t  ._  / 
                  "-.t-._:'
 
-  `)
+  `);
 });
 
-app.listen(app.get('port'), () => console.log(`Server running on ${app.get('port')}`));
+app.listen(app.get("port"), () =>
+  console.log(`Server running on ${app.get("port")}`)
+);
